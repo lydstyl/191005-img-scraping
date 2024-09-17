65ae2b64bda88a67ffb07fa620117e69d29206bd
Gabriel Brun <lydstyl@gmail.com>
2024-09-17 09:16:24 +0200

    master branch

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const images = require('./images.js')
 
 const saveImages = (images) => {
   // remove duplicates then save images
+  // master
   ;[...new Set(images)].forEach((img, index) => {
     request(img).pipe(fs.createWriteStream(`./images/${index}.jpeg`))
   })
