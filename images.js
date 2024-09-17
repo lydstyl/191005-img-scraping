ba3d13e958e70e5043f07c0f8fd206f803e066f8
Gabriel Brun <lydstyl@gmail.com>
2024-09-17 09:14:08 +0200

    Remove duplicate then save images.

---
+++ images.js
@@ -0,0 +1,5 @@
+module.exports = [
+  'https://2023-923ce00f-e7c6-499a-96bf-ff03fbab8e82-gi.s3.toutemonannee.com/n1/42d25347-40b6-4510-8f06-b1037b39aaf8/hd/f06106d1-3942-46bc-a9b0-3f0723a95232.jpg?lastmod=1721985322',
+  'https://2023-923ce00f-e7c6-499a-96bf-ff03fbab8e82-gi.s3.toutemonannee.com/n1/42d25347-40b6-4510-8f06-b1037b39aaf8/hd/2e7c34b7-a274-41a0-a5bf-e97a760c93db.jpg?lastmod=1709890602'
+]
+// document.querySelectorAll('img.lg-image').forEach(img => console.log(img.src))
---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const request = require('request')
+const images = require('./images.js')
+
+const saveImages = (images) => {
+  // remove duplicates then save images
+  ;[...new Set(images)].forEach((img, index) => {
+    request(img).pipe(fs.createWriteStream(`./images/${index}.jpeg`))
+  })
+}
+
+saveImages(images)
